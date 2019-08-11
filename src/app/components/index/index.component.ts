c049f10133943d4deb19cb2d28ebc692b7d36daf
whenofficial <fsmitchm@gmail.com>
2019-08-11 15:29:21 +0800

    Bills and more UI

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './index.component.html',
 })
 export class IndexComponent implements OnInit {
-  public isProd = false;
+  public isProd = true;
 
   constructor() { }
 
